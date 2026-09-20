feca397aef9c1b60f3ee3b17f3dda667ba552592
Dimitri Azzarone <LA_TUA_EMAIL_GITHUB>
2026-09-20 21:12:42 +0200

    Dan private desktop mode

--- app/src/main/java/com/dimitriazzarone/chatgptmessenger/MainActivity.java
+++ app/src/main/java/com/dimitriazzarone/chatgptmessenger/MainActivity.java
@@ -477,6 +477,11 @@ public class MainActivity extends Activity {
             settings.setDisplayZoomControls(false);
             settings.setUseWideViewPort(true);
             settings.setLoadWithOverviewMode(true);
+            settings.setUserAgentString(
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                    "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                    "Chrome/140.0.0.0 Safari/537.36"
+            );
             settings.setMixedContentMode(
                     WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
             );
